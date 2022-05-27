4ebcdddbc0ca4a869e16e016ab4ecb50d7f0c889
IgnacioLF <100155212+IgnacioLF@users.noreply.github.com>
2022-05-27 08:31:19 +0200

    -added singin with google

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes,Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Homepage from './pages/Homepage.jsx';
 import Registration from './pages/Registration.jsx';
+import Loginpage from './pages/Loginpage.jsx';
 import './default.scss';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
             <Routes>
                 <Route exact path="/" element={<Homepage />}/>
                 <Route path="/register" element={<Registration />}/>
+                <Route path="/login" element={<Loginpage />}/>
             </Routes>
         </div>
      </div>
